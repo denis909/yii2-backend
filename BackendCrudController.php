e08404b79a16874be8f7732d9776f4296c244280
denis909 <denis909@mail.ru>
2020-08-04 22:36:34 +0300

    n/a

--- BackendCrudController.php
+++ BackendCrudController.php
@@ -2,66 +2,11 @@
 
 namespace denis909\backend;
 
-use denis909\yii\IndexAction;
-use denis909\yii\CreateAction;
-use denis909\yii\UpdateAction;
-use denis909\yii\DeleteAction;
-
-abstract class BackendCrudController extends BackendController
+abstract class BackendCrudController extends \denis909\yii\CrudController
 {
 
-    public $defaultAction = 'index';
+    public $userComponent = 'user';
 
-    public $modelClass;
-
-    public $searchModelClass;
-
-    public $formModelClass;
-
-    public $ownerClass;
-
-    public $ownerIndex = 'ownerId';
-
-    public $parentId;
-
-    public $postActions = ['delete'];
-
-    public $dataProvider = []; 
-
-    public $i18nCategory = 'backend';
-
-    public function actions()
-    {
-        return [
-            'index' => [
-                'class' => IndexAction::class,
-                'i18nCategory' => $this->i18nCategory,
-                'modelClass' => $this->modelClass,
-                'searchModelClass' => $this->searchModelClass,
-                'ownerClass' => $this->ownerClass,
-                'parentId' => $this->parentId,
-                'dataProvider' => $this->dataProvider,
-                'ownerIndex' => $this->ownerIndex
-            ],
-            'create' => [
-                'class' => CreateAction::class,
-                'i18nCategory' => $this->i18nCategory,
-                'modelClass' => $this->formModelClass,
-                'ownerClass' => $this->ownerClass,
-                'parentId' => $this->parentId,
-                'ownerIndex' => $this->ownerIndex
-            ],
-            'update' => [
-                'class' => UpdateAction::class,
-                'i18nCategory' => $this->i18nCategory,
-                'modelClass' => $this->formModelClass
-            ],
-            'delete' => [
-                'class' => DeleteAction::class,
-                'i18nCategory' => $this->i18nCategory,
-                'modelClass' => $this->modelClass
-            ]
-        ];
-    }
+    public $roles = ['@'];
 
 }
